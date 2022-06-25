16930182f2cae50ea4afdbe54d47332c5a23b0a6
saboorahm <saboorahmadsatti2@gmail.com>
2022-06-25 20:49:25 +0500

    initialized git

---
+++ Git1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git1
+//
+//  Created by Saboor on 25/06/2022.
+//  Copyright © 2022 Saboor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
