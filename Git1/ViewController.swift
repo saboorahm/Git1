55f0a565a4a946afa3d30bd30a024b275ec6f412
saboorahm <saboorahmadsatti2@gmail.com>
2022-06-25 20:53:20 +0500

    add fuction

--- Git1/ViewController.swift
+++ Git1/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func add(){
+        print("adding....")
+    }
 
 
 }
